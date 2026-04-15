dce638f96fef6fa6661c4e9162b301af8e822438
Deepak Koshta <98534103+d-p-k@users.noreply.github.com>
2026-04-15 16:44:16 +0530

    Refactor datasource URL retrieval method

--- prisma.config.ts
+++ prisma.config.ts
@@ -9,7 +9,8 @@ export default defineConfig({
     path: "prisma/migrations",
   },
   datasource: {
-    url: process.env["DATABASE_URL"] || "",
+    // url: process.env["DATABASE_URL"] || "",
+    url: env("DATABASE_URL"),
     // url: process.env.DATABASE_URL || "",
   },
 });
